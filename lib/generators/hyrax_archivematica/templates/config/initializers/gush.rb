e3ec3d346cebcb00f5bebc139f612a6725472a0e
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2022-06-22 12:05:00 +0000

    add gush config

---
+++ lib/generators/hyrax_archivematica/templates/config/initializers/gush.rb
@@ -0,0 +1,12 @@
+# config/initializers/gush.rb
+#Gush.configure do |config|
+#  config.redis_url = "redis://localhost:6379"
+#  config.concurrency = 5
+#  config.locking_duration = 2 # how long you want to wait for the lock to be released, in seconds
+#  config.polling_interval = 0.3 # how long the polling interval should be, in seconds
+#end
+
+Gush.configure do |config|
+  config.redis_url = "redis://#{ENV.fetch('REDIS_HOST', 'localhost')}:#{ENV.fetch('REDIS_PORT', '6379')}"
+  config.concurrency = 1
+end
